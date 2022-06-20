3fced5f2fb4b2f651797567d07e84d368e4a662b
roam-edge <3336851348@qq.com>
2022-06-21 00:47:48 +0800

    commit

--- assets/index.840c34e1.js
+++ assets/index.840c34e1.js
@@ -1,2 +1,1 @@
 import{bv as t,bw as s,aO as a}from"./index.05095889.js";const u={setup(){const e=t(),r=s();e.replace(r.fullPath.replace(/^\/redirect/,""))},render(){return a("div")}};export{u as default};
-//# sourceMappingURL=index.840c34e1.js.map
